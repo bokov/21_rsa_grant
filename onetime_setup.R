7d4a97e702e6033cc8683f4d29a0b52f665288eb
Alex Bokov <bokov@uthscsa.edu>
2021-09-24 17:48:10 -0500

    onetime_setup.R; deploy; A script that installs the needed libraries for this project. As the name says, it only needs to be run once per computer.

---
+++ onetime_setup.R
@@ -0,0 +1,20 @@
+# required CRAN packages
+install.packages(.crandeps<-c('rio','tidyverse','devtools','pander','rmarkdown'
+                              ,'GGally','Boruta','nFactors','metamedian')
+                 ,repos = 'https://cloud.r-project.org');
+
+# required author package
+devtools::install_github('bokov/tidbits',ref='integration');
+
+# Optional CRAN packages. Uncomment the below lines in order to install them
+# install.packages(.cranoptional<-c('synthpop','GGally')
+#                  ,repos='https://cloud.r-project.org');
+
+# Testing install
+.results <- sapply(c(.crandeps,'tidbits'),require,character.only=T);
+if(!all(.results)){
+  message('Problems found. Before you will be able to run these scripts you will need to figure out why the following packages failed to install:\n    '
+          ,paste0(names(.results[!.results]),collapse=', '))
+} else {
+  message('All required packages successfully installed or updated.');
+}
