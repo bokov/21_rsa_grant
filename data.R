c73b053adfe30d8e484ba4e548cbd5c894063785
Alex Bokov <bokov@uthscsa.edu>
2021-10-16 12:52:06 -0500

    data.R; tweak; Changed the the saved files to their proper default names.

--- data.R
+++ data.R
@@ -27,8 +27,8 @@ inputdata <- c(dat0='data/SIM_SDOH_ZCTA.xlsx'          # census data by ZCTA
                ,dct0='data/data_dictionary.csv'        # data dictionary for the
                                                        # dat1 dataset that _this_
                                                        # scriport produces
-               ,dat1='SDOH_RSR_2013_prelim.csv'        # the dat1 dataset
-               ,dat2='SDOH_RSR_2013_scaled_prelim.csv' # the scaled version of
+               ,dat1='SDOH_RSR_201X_prelim.csv'        # the dat1 dataset
+               ,dat2='SDOH_RSR_201X_scaled_prelim.csv' # the scaled version of
                                                        # dat1
 );
 
