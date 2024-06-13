004f9ce3b5de57ed5a5ce53afe7e89a790608665
Alex Bokov <bokov@uthscsa.edu>
2024-06-13 09:40:55 -0500

    config.R; config; Adds sdohyear variable and a colmap item to inputdata

--- config.R
+++ config.R
@@ -1,5 +1,7 @@
 project_seed <- 2021091045;
 
+sdohyear <- '2020';
+
 inputdata <- c(dat0= 'data/SIM_SDOH_ZCTA.xlsx'         # census data by ZCTA
                ,cx0= 'data/SIM_ALLCMS.csv'             # RSA-ZCTA crosswalk
                ,rsa0='data/SIM_RSAv4 SCD RSRs.csv'     # outcomes (RSR)
@@ -9,4 +11,11 @@ inputdata <- c(dat0= 'data/SIM_SDOH_ZCTA.xlsx'         # census data by ZCTA
                ,dat1='SDOH_RSR_201X_prelim.csv'        # the dat1 dataset
                ,dat2='SDOH_RSR_201X_scaled_prelim.csv' # the scaled version of
                                                        # dat1
+               ,colmap='data/sdohmapping_ZCTA2ZIP.csv' # columns from beta zcta
+                                                       # version and their
+                                                       # production zip version
+                                                       # equivalents
 );
+
+
+c()
